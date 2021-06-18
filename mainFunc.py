db7b5137615250f17987507a09fbda6a296c63b8
micheledellipaoli <m.dellipaoli1@studenti.unisa.it>
2021-06-18 11:24:46 +0200

    mainFunc
    First commit of mainFunc.py

---
+++ mainFunc.py
@@ -0,0 +1,39 @@
+import snap
+
+
+def set_static_treshold(hash_table, value):
+    for key in hash_table:
+        hash_table[key] = value
+    return hash_table
+
+
+def set_degree_based_treshold(hash_table, degrees):
+    for key in hash_table:
+        hash_table[key] = degrees[key]
+    return hash_table
+
+
+def exec():
+    # Load Graph
+    # noinspection PyUnresolvedReferences
+    graph = snap.LoadEdgeListStr(snap.TUNGraph, "dataset/twitch/ENGB/musae_ENGB_edges.csv", 0, 1)
+
+    # Get the degree of each node by storing them into an hash-table
+    # noinspection PyUnresolvedReferences
+    degrees = graph.GetDegSeqV()
+    for i in range(0, degrees.Len()):
+        print("Node %s has degree %s" % (i, degrees[i]))
+
+    # Creates an hash-table used to store the treshold for each node.
+    # noinspection PyUnresolvedReferences
+    t = snap.TIntH()
+
+    # Set the treshold for each node statically
+    set_static_treshold(t, 1)
+
+    # Set the treshold for each node based on its degree
+    set_degree_based_treshold(t, degrees)
+
+
+if __name__ == "__main__":
+    exec()
